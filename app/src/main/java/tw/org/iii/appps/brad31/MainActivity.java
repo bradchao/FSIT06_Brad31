af018357f87ca63d3918ff3d3da3a32cfc74576e
Brad <brad@brad.tw>
2019-08-24 09:25:55 +0800

    1

---
+++ app/src/main/java/tw/org/iii/appps/brad31/MainActivity.java
@@ -0,0 +1,54 @@
+package tw.org.iii.appps.brad31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        initListView();
+    }
+
+    private void initListView (){
+        adapter = new MyAdapter();
+        listView.setAdapter(adapter);
+    }
+
+    private class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
+
+
+
+}
